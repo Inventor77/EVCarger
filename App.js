f219604b7f4a3ead4fca6291ac1c71e42ed843cd
Shreedutt <shreedutt77@gmail.com>
2022-04-23 18:19:27 +0530

    card is developed

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import MapView, { Marker } from "react-native-maps";
-// import NavigationIcon from "./src/Icons/NavigationIcon";
 import {
 	StyleSheet,
 	View,
 	StatusBar,
 	Dimensions,
 	Animated,
+	Text
 } from "react-native";
 import Svg, { Path } from "react-native-svg";
 import { keyframes, stagger } from "popmotion";
@@ -19,7 +19,7 @@ export default function App() {
 	const [animations, setAnimations] = useState(
 		Array(COUNT).map(() => INITIAL_PHASE)
 	);
-
+	const data = require("./data.json");
 	const animate = () => {
 		const actions = Array(COUNT).fill(
 			keyframes({
@@ -34,7 +34,9 @@ export default function App() {
 		});
 	};
 
-	useEffect(() => animate(), []);
+	useEffect(() => {
+		animate();
+	}, []);
 
 	return (
 		<View style={styles.container}>
@@ -71,14 +73,12 @@ export default function App() {
 							);
 						})}
 						<View style={styles.marker}>
-							{/* <NavigationIcon style={styles.markerImage} /> */}
 							<Svg
 								xmlns="http://www.w3.org/2000/svg"
 								width={18}
 								height={20}
 								viewBox="0 0 24 24"
 								fill="#fff"
-								// {...props}
 							>
 								<Path d="M10.8 6.2C6.5 7.9 3 9.8 3 10.6c0 .7 1.5 1.8 3.4 2.5 2.3.8 3.7 2.2 4.5 4.5.7 1.9 1.8 3.4 2.5 3.4C14.5 21 21 6.4 21 3.9c0-1.4-1.8-1-10.2 2.3z" />
 							</Svg>
@@ -86,6 +86,13 @@ export default function App() {
 					</View>
 				</Marker>
 			</MapView>
+			{
+				data.chargers.map((info) =>
+					<Card 
+					info = {info}
+					/>
+					)
+			}
 		</View>
 	);
 }
@@ -116,6 +123,7 @@ const styles = StyleSheet.create({
 		alignItems: "center",
 		position: "absolute",
 		zIndex: 1,
+		transform: [{ scaleX: -1 }],
 	},
 	markerImage: {},
 	circle: {
@@ -126,4 +134,60 @@ const styles = StyleSheet.create({
 		alignSelf: "center",
 		position: "absolute",
 	},
+});
+
+function Card({info}) {
+	return (
+		<View style={cardStyles.container}>
+			<Text style={cardStyles.header}>{info.name.toUpperCase().split("-")[0]}...</Text>
+			<Text style={cardStyles.subHeader} >{info.address}</Text>
+			<Text style={cardStyles.title}>SUPPORTED CONNECTORS</Text>
+			<View>
+			{info.connector_types.map((connector, idx) => 
+				<View key={idx}>
+					<Text style={cardStyles.connector} >
+						{connector[0] === "l"
+							? 
+							"Level " + connector.split("-")[0][3] + (" ") + connector.split("-")[0].substring(connector.split("-")[0].length - 2).toUpperCase()  
+							: 
+							"Normal " + connector.split("-")[0].substring(connector.split("-")[0].length - 2).toUpperCase()
+						}
+					</Text>
+					<Text style={cardStyles.connectorCnt}> X{ connector.split("-")[1]}</Text>
+				</View>
+			)}
+			</View>			
+		</View>
+	);
+}
+
+const cardStyles = StyleSheet.create({
+	container: {
+		position: "absolute",
+		height: 280,
+		width: 240,
+		backgroundColor: "#000",
+		borderRadius: 16,
+		bottom: 16,
+		left: 16,
+	},
+	header: {
+		fontSize: 15,
+		color: "#fff",
+	},
+	subHeader: {
+		textTransform: "capitalize",
+		color: "#454545"
+	},
+	title: {
+		textTransform: "uppercase",
+		color: "#68BFA1"
+	},
+	connector: {
+		color: "#fff",
+
+	},
+	connectorCnt: {
+		color: "#fff"
+	}
 });
